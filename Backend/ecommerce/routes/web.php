099b2ce861310291c363c8af9b57d763f36798c8
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-07-14 23:42:16 -0500

    ui bootstrap installed, auth created, but npm run dev still will not compile source css and js, have to add bootstrap CDN in <head> manully

--- Backend/ecommerce/routes/web.php
+++ Backend/ecommerce/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/test', [HomeController::class, 'test']);
