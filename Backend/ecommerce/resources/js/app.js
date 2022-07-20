838b6b464c9e264f008de5c4d5b2a69721a1d66a
Pui Ching Chung <lawchunni@yahoo.com.hk>
2022-07-20 18:17:18 -0500

    [Backend] navigation menu dropdown updated fron frontend

--- Backend/ecommerce/resources/js/app.js
+++ Backend/ecommerce/resources/js/app.js
@@ -1,27 +1,39 @@
 import "./bootstrap";
+
 'use strict';
 
-$(document).ready(function () {
+$(document).ready(function() {
     // menu
-    menuDropdown(); // footer
-
+    menuDropdown();
+    // footer
     getCurrentYear();
 });
+
 /**
  * Display currect year for footer
  */
-
 function getCurrentYear() {
     $('#footer_year').text(new Date().getFullYear());
 }
 
 function menuDropdown() {
-    $('.icon.profile a').mouseover(function (e) {
+    $('.icon.profile a, .profile_dropdown').mouseover(function(e) {
         e.preventDefault();
-        $('.profile_dropdown').stop().fadeIn(300);
+        $('.profile_dropdown').stop().fadeIn(200);
     });
-    $('.icon.profile a').mouseleave(function (e) {
+
+    $('.icon.profile a, .profile_dropdown').mouseleave(function(e) {
         e.preventDefault();
-        $('.profile_dropdown').stop().fadeOut(300);
+        $('.profile_dropdown').stop().fadeOut(200);
+    });
+
+    $('nav ul li a, .nav_dropdown').mouseover(function(e) {
+        e.preventDefault();
+        $('.nav_dropdown').stop().fadeIn(200);
+    });
+
+    $('nav ul li a, .nav_dropdown').mouseleave(function(e) {
+        e.preventDefault();
+        $('.nav_dropdown').stop().fadeOut(200);
     });
 }
