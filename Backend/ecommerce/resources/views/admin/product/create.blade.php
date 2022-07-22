3c014fcb53ddf7d3a31e544322590b6c7320c993
dhruvalviradiya <dhruvalviradiya@gmail.com>
2022-07-22 00:46:11 -0500

    [Backend] Create Product from admin panel

---
+++ Backend/ecommerce/resources/views/admin/product/create.blade.php
@@ -0,0 +1,149 @@
+@extends('layouts/app')
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <h2 class="mb-0">Add Post</h2>
+                </div>
+                <div class="card-body">
+                    {{ $errors }}
+                    <form action="{{ route('admin_product_save') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                                name="name" placeholder="Enter Name" value="{{ old('name') }}">
+                            @error('name')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="slug" class="form-label">Slug</label>
+                            <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug"
+                                name="slug" placeholder="Enter Slug" value="{{ old('slug') }}">
+                            @error('slug')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="color" class="form-label">Color</label>
+                            <input type="text" class="form-control @error('color') is-invalid @enderror" id="color"
+                                name="color" placeholder="Enter Color" value="{{ old('color') }}">
+                            @error('color')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="price" class="form-label">Price</label>
+                            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price"
+                                name="price" placeholder="Enter Price" value="{{ old('price') }}">
+                            @error('price')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="quantity" class="form-label">Quantity</label>
+                            <input type="text" class="form-control @error('quantity') is-invalid @enderror"
+                                id="quantity" name="quantity" placeholder="Enter Quantity" value="{{ old('quantity') }}">
+                            @error('quantity')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="images" class="form-label">Image</label>
+                            <input type="file" name="images[]" multiple class="form-control" accept="image/*">
+                            @if ($errors->has('images'))
+                                @foreach ($errors->get('images') as $error)
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $error }}</strong>
+                                    </span>
+                                @endforeach
+                            @endif
+                            {{-- <input type="file" class="form-control @error('image') is-invalid @enderror" id="image"
+                                name="image">
+                            @error('image')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror --}}
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Description</label>
+                            <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"
+                                rows="3" placeholder="Enter Description">{!! old('description') !!}</textarea>
+                            @error('description')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="category_id" class="form-label">Category</label>
+                            <select multiple class="form-select @error('category_id') is-invalid @enderror"
+                                name="category_id[]">
+                                @foreach ($categories as $index => $cat)
+                                    <option value="{{ $index }}"
+                                        {{ collect(old('category_id'))->contains($index) ? 'selected' : '' }}>
+                                        {{ $cat->title }}</option>
+                                @endforeach
+                            </select>
+                            @error('category_id')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="gender_id" class="form-label">Gender</label>
+                            <select class="form-select @error('gender_id') is-invalid @enderror" id="gender_id"
+                                name="gender_id">
+                                {{-- <option value="">Select Gender</option> --}}
+                                @foreach ($genders as $index => $item)
+                                    <option value="{{ $index }}"
+                                        {{ old('gender_id') == $index ? 'selected' : '' }}>{{ $item->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('gender_id')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="size_id" class="form-label">Size</label>
+                            <select class="form-select @error('size_id') is-invalid @enderror" id="size_id"
+                                name="size_id">
+                                {{-- <option value="">Select Size</option> --}}
+                                @foreach ($sizes as $index => $item)
+                                    <option value="{{ $index }}" {{ old('size_id') == $index ? 'selected' : '' }}>
+                                        {{ $item->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('size_id')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <input type="submit" class="btn btn-primary">
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
--- Backend/ecommerce/routes/dhruval-route.php
+++ Backend/ecommerce/routes/dhruval-route.php
@@ -3,6 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
+// admin panel
+Route::get('/admin/product', [App\Http\Controllers\Admin\ProductController::class, 'index'])
+    ->name('admin_product_list');
+Route::get('/admin/product/create', [App\Http\Controllers\Admin\ProductController::class, 'create'])
+    ->name('admin_product_add');
+Route::post('/admin/product', [App\Http\Controllers\Admin\ProductController::class, 'store'])
+    ->name('admin_product_save');
 
 Route::get('/product', [ProductController::class, 'index'])
     ->name('product_list');
