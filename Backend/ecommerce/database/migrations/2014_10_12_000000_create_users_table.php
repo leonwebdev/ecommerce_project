4955503fca7a1c826e78abc684284cbfcb5ff35a
dhruvalviradiya <dhruvalviradiya@gmail.com>
2022-07-22 10:13:12 -0500

    [Backend] Timestamp related correction in migration

--- Backend/ecommerce/database/migrations/2014_10_12_000000_create_users_table.php
+++ Backend/ecommerce/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -26,7 +27,8 @@ return new class extends Migration
             $table->boolean('admin')->default(false);
             $table->softDeletes();
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
--- Backend/ecommerce/database/migrations/2022_07_17_205913_create_products_table.php
+++ Backend/ecommerce/database/migrations/2022_07_17_205913_create_products_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
-            $table->string('slug', 255);
+            $table->string('slug', 255)->unique();
             $table->string('color', 255);
             $table->text('summary');
             $table->longText('description');
