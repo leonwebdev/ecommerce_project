125daaa67ae3b9a3322643a0f68a84f6b7a1ac94
leonwebdev <109298601+leonwebdev@users.noreply.github.com>
2022-07-21 21:12:28 -0500

    [Backend]user profile controller and route created, but need html template to finish this page

--- Backend/ecommerce/routes/lihang-route.php
+++ Backend/ecommerce/routes/lihang-route.php
@@ -3,9 +3,13 @@
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\ProfileController;
 
 
 // Route::get('/test', [HomeController::class, 'test']);
+
 Route::get('/ltest', function () {
     return User::find(2)->user_addresses;
 });
+
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
