a46903e4efc27d8d5aab7a7c4f8f929d0a3560c0
dhruvalviradiya <dhruvalviradiya@gmail.com>
2022-07-25 09:46:58 -0500

    [Backend] CSRF Token and jquery added in admin panel

--- Backend/ecommerce/resources/views/layouts/admin.blade.php
+++ Backend/ecommerce/resources/views/layouts/admin.blade.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel | {{ $title }}</title>
-
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Styles -->
     <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 </head>
+
 <body>
     @include('admin/partials/header')
     <!--Main layout-->
@@ -23,4 +27,5 @@
 
     <script src="https://kit.fontawesome.com/c89d7d6e41.js" crossorigin="anonymous"></script>
 </body>
+
 </html>
