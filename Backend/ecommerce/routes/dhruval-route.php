027f3f457f0165d5267908521810e814e97f7e7c
dhruvalviradiya <dhruvalviradiya@gmail.com>
2022-07-25 22:54:07 -0500

    [Backend] Admin Home page

--- Backend/ecommerce/routes/dhruval-route.php
+++ Backend/ecommerce/routes/dhruval-route.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 // admin panel
+
+// product start
 Route::get('/admin/product', [App\Http\Controllers\Admin\ProductController::class, 'index'])
     ->name('admin_product_list');
 Route::get('/admin/product/create', [App\Http\Controllers\Admin\ProductController::class, 'create'])
@@ -18,6 +20,10 @@ Route::delete('/admin/product/{product}', [App\Http\Controllers\Admin\ProductCon
     ->name('admin_product_delete');
 Route::delete('/admin/product/media/{media}', [App\Http\Controllers\Admin\ProductController::class, 'deleteMedia'])
     ->name('admin_product_media_delete');
+// product end
+
+Route::get('/admin/home', [App\Http\Controllers\Admin\HomeController::class, 'index'])
+    ->name('admin_home');
 
 Route::get('/product', [ProductController::class, 'index'])
     ->name('product_list');
