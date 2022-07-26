c3fa34fb116dc9a2bfe85b3c4c8f095843a0d216
leonwebdev <109298601+leonwebdev@users.noreply.github.com>
2022-07-26 14:33:26 -0500

    [Backend]Revise order history list view and detail view.

--- Backend/ecommerce/resources/views/auth/order_history/index.blade.php
+++ Backend/ecommerce/resources/views/auth/order_history/index.blade.php
@@ -15,20 +15,20 @@
                             <h3 class="text-align-center my-3 mx-3">You have no orders yet. <a href="/product" id="shopping_now">Shopping Now.</a></h3>
                         @else
                             @foreach ($orders as $key => $order)
-                                <div class="order_container mx-1 mb-5">
+                                <div class="order_container mx-1 mb-7">
                                     <div class="order_heading d-flex justify-content-space-between align-items-center">
-                                        <h2 class="fw-bold mb-1">&#35; Order {{ $key+1 }}</h2>
+                                        <h2 class="fw-bold my-0">&#35; Order {{ $key+1 }}</h2>
                                         <div><a href="" class="lh-btn no-hover text-transform-capitalize"
                                                 onclick="event.preventDefault();"
                                                 >{{ $order->order_status }}</a></div>
                                     </div>
-                                    <div class="product_list">
-                                        <div class="d-flex justify-content-space-between my-2 mb-1">
-                                            <div class="mb-0_5 text-align-left"><span class="text-black fw-bold">TOTAL: </span>&#36;{{ $order->total }}</div>
-                                            <div class="mb-0_5 text-align-right"><a href="{{ route('order-history-detail', ['id' => $order->id]) }}"
+                                    <div class="product_list min-width-40">
+                                        <div class="text-align-left"><span class="text-black fw-bold">TOTAL: </span>&#36;{{ $order->total }}</div>
+                                        <div class="d-flex justify-content-space-between my-1 align-items-center">
+                                            <div class=""><span class="text-black fw-bold">Order Date: </span>{{ $order->created_at }}</div>
+                                            <div class="text-align-right"><a href="{{ route('order-history-detail', ['id' => $order->id]) }}"
                                                                                         class="lh-btn fs-1">Order Detail</a></div>
                                         </div>
-                                        <div class="mb-0_5"><span class="text-black fw-bold">Shipping address: </span>{{ $order->shipping_address }}</div>
                                     </div>
                                 </div>
                             @endforeach
