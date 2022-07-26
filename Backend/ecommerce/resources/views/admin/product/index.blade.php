0fe2c8a39fc0b1d9e17403abf199593acce66c1b
dhruvalviradiya <dhruvalviradiya@gmail.com>
2022-07-25 20:23:50 -0500

    [Backend] Search form in admin product listing

--- Backend/ecommerce/resources/views/admin/product/index.blade.php
+++ Backend/ecommerce/resources/views/admin/product/index.blade.php
@@ -6,12 +6,14 @@
 
         <!-- Search form -->
         <div class="search_form">
-            <form class="d-md-flex input-group w-auto my-auto">
-                <input autocomplete="off" type="search" class="form-control rounded" placeholder='' style="min-width: 225px" />
-                <span class="input-group-text border-0">
-                    <i class="fas fa-search"></i></span>
+            <form class="d-md-flex input-group w-auto my-auto" action="{{ route('admin_product_list') }}" method="get">
+                <input autocomplete="off" type="search" class="form-control rounded" name="search" placeholder='Search'
+                    maxlength="255" style="min-width: 225px" />
+                <button class="input-group-text border-0" type="submit">
+                    <i class="fas fa-search"></i></button>
             </form>
         </div>
+
     </div>
     <a href="{{ route('admin_product_add') }}" class="btn btn-primary">Add a Product</a>
 
