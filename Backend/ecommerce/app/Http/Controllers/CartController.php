074be4348a5722d180028c6cbb9fc095666d1414
Pui Ching Chung <lawchunni@yahoo.com.hk>
2022-07-26 16:58:29 -0500

    [Backend] added addToCart function to store cart in session

--- Backend/ecommerce/app/Http/Controllers/CartController.php
+++ Backend/ecommerce/app/Http/Controllers/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class CartController extends Controller
 {
@@ -18,5 +19,27 @@ class CartController extends Controller
         return view('cart/index', compact(
             'title', 
         ));
+    }
+
+    /**
+     * Add to Cart from product detail page logic
+     *
+     * @param Request $request
+     * @param [type] $id product id
+     * @return void
+     */
+    public function addToCart(Request $request, $id) {
+        $session = $request->session()->get('cart');
+        if( isset($session[$id]) ) {
+            $session[$id] = $session[$id] + 1;
+        } else {
+            $session[$id] = 1;
+        }
+
+        $request->session()->put('cart', $session);
+
+        $prev_url = url()->previous();
+
+        return redirect($prev_url);
     }
 }
--- Backend/ecommerce/routes/jenny-route.php
+++ Backend/ecommerce/routes/jenny-route.php
@@ -6,3 +6,4 @@ use App\Http\Controllers\CartController;
 
 // Route::get('/test', [HomeController::class, 'test']);
 Route::get('/cart', [CartController::class, 'index'])->name('cartIndex');
+Route::get('/cart/add/{id}', [CartController::class, 'addToCart'])->name('addToCart');
