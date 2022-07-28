668598927a66757f0887af6932de3296cbdd482c
leonwebdev <109298601+leonwebdev@users.noreply.github.com>
2022-07-27 22:44:40 -0500

    [Backend]Revise Tax Seeder.

--- Backend/ecommerce/database/seeders/TaxTableSeeder.php
+++ Backend/ecommerce/database/seeders/TaxTableSeeder.php
@@ -16,9 +16,105 @@ class TaxTableSeeder extends Seeder
     public function run()
     {
         DB::table('taxes')->insert([
+            'province' => 'AB',
+            'gst' => 0.05,
+            'pst' => 0,
+            'hst' => 0,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('taxes')->insert([
             'province' => 'BC',
             'gst' => 0.05,
             'pst' => 0.07,
+            'hst' => 0,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('taxes')->insert([
+            'province' => 'MB',
+            'gst' => 0.05,
+            'pst' => 0.07,
+            'hst' => 0,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('taxes')->insert([
+            'province' => 'NB',
+            'gst' => 0,
+            'pst' => 0,
+            'hst' => 0.15,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('taxes')->insert([
+            'province' => 'NL',
+            'gst' => 0,
+            'pst' => 0,
+            'hst' => 0.15,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('taxes')->insert([
+            'province' => 'NT',
+            'gst' => 0.05,
+            'pst' => 0,
+            'hst' => 0,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('taxes')->insert([
+            'province' => 'NS',
+            'gst' => 0,
+            'pst' => 0,
+            'hst' => 0.15,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('taxes')->insert([
+            'province' => 'NU',
+            'gst' => 0.05,
+            'pst' => 0,
+            'hst' => 0,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('taxes')->insert([
+            'province' => 'ON',
+            'gst' => 0,
+            'pst' => 0,
+            'hst' => 0.13,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('taxes')->insert([
+            'province' => 'PE',
+            'gst' => 0,
+            'pst' => 0,
+            'hst' => 0.15,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('taxes')->insert([
+            'province' => 'QC',
+            'gst' => 0.05,
+            'pst' => 0.09975,
+            'hst' => 0,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('taxes')->insert([
+            'province' => 'SK',
+            'gst' => 0.05,
+            'pst' => 0.06,
+            'hst' => 0,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('taxes')->insert([
+            'province' => 'YT',
+            'gst' => 0.05,
+            'pst' => 0,
             'hst' => 0,
             'created_at' => now(),
             'updated_at' => now(),
