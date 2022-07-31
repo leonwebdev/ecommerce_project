275f3b38ffd9ae6217ec0ff76cad953e25ad4e38
leonwebdev <109298601+leonwebdev@users.noreply.github.com>
2022-07-30 20:52:17 -0500

    Invoice email test succeed.

---
+++ Backend/ecommerce/app/Mail/Invoice.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Invoice extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The order instance.
+     *
+     * @var \App\Models\Order
+     */
+    public $order;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $order = $this->order;
+        $user = User::find($order->user_id);
+        return $this->view('emails.orders.invoice')
+            ->with([
+                'order' => $order,
+                'user' => $user,
+            ]);
+    }
+}
---
+++ Backend/ecommerce/resources/views/emails/orders/invoice.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            background-color: rgb(180, 180, 184);
+        }
+
+        .container {
+            margin: 16px auto;
+            margin-bottom: 64px;
+            width: 640px;
+            background-color: #fff;
+            border-radius: 8px;
+            min-height: 800px;
+        }
+
+        h1 {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Invoice</h1>
+    </div>
+</body>
+
+</html>
