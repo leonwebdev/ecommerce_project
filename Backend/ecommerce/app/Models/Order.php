bf45c58b537357c0d274245e4f2cf5273766bcc5
dhruvalviradiya <dhruvalviradiya@gmail.com>
2022-08-04 23:57:59 -0500

    [bug fix] fixed issue related to deleted user and product in order list

--- Backend/ecommerce/app/Models/Order.php
+++ Backend/ecommerce/app/Models/Order.php
@@ -38,7 +38,7 @@ class Order extends Model
      */
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 
     /**
